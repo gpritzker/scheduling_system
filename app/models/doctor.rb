2cd7a1ccabfa4327934d985f7bcb1a4e348a3090
Gonzalo Pritzker <gonzalopritzker@gp-2.local>
2023-09-10 18:06:19 -0300

    fix: change who verify the laborable day for the doctor when create a appoitment

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -8,11 +8,16 @@ class Doctor < ApplicationRecord
         #convert start_time and end_time to Time objects
         # requested_start_time = Time.parse(start_time)
         # requested_end_time = Time.parse(end_time)
+        date_object = Date.parse(date)
+        weekday = date_object.strftime('%A').upcase 
         requested_start_time = start_time
         requested_end_time = end_time
-        # Verificar que el end_time sea posterior al start_time
+        # Verify that the requested time range is valid
         return false if requested_start_time >= requested_end_time
-
+        availability = Availability.find_by(doctor_id: doctor_id, weekday: weekday)
+        # Verify that the requested date is a laborable date for the doctor
+        return false if availability.nil?
+        
         # Search if the doctor already has an appointment in the requested time range
         overlapping_appointments = appointments.where(date: date).where("(start_time <= ? AND end_time > ?) OR (start_time < ? AND end_time >= ?) OR (start_time >= ? AND end_time <= ?)", requested_start_time, requested_start_time, requested_end_time, requested_end_time, requested_start_time, requested_end_time)
         #if overlapping appoitmens the doctor is not available
